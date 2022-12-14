e012c2f4c0eac5468f7e812e1f65f7f6234dc2ed
Jonathan Marquez <zomodan2306@gmail.com>
2022-12-14 10:38:10 -0600

    Feat(styles): added support for tailwind in the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1>Page Layout</h1>
+      <h1 className="text-3xl">Page Layout</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
