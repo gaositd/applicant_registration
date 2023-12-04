b6c122b23964b7975714da8ae730abd9f356ef8d
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-12-03 20:56:48 -0600

    validación sección 1 lista

--- components/pages/dashboard/register/validation.schema.ts
+++ components/pages/dashboard/register/validation.schema.ts
@@ -6,6 +6,16 @@ enum Sexo {
   otro = "otro",
 }
 
+enum EstadoCivil {
+  casado = "casado",
+  soltero = "soltero",
+}
+
+enum Dialecto {
+  si = "si",
+  no = "no",
+}
+
 const StepOneValidationSchema = z.object({
   nombre: z.string().min(2, {
     message: "El nombre debe tener al menos 2 caracteres.",
@@ -17,6 +27,47 @@ const StepOneValidationSchema = z.object({
     message: "El apellido materno debe tener al menos 2 caracteres.",
   }),
   sexo: z.nativeEnum(Sexo, {
+    errorMap: (issue, ctx) => {
+      console.log(issue, ctx);
+      if (issue.code === "invalid_enum_value")
+        return {
+          message: "Opción no válida.",
+        };
+      return {
+        message: "Debes seleccionar una opción.",
+      };
+    },
+  }),
+  fechaNacimiento:z.date().refine(fechaIngresada =>{
+    const fechaLimite = new Date();
+    //calcula la efcha 15 años hacia atras para poder ser una fecha válida
+    fechaLimite.setFullYear(fechaLimite.getFullYear() - 15);
+    return fechaIngresada <= fechaLimite;
+  },
+  {
+    message: `La fecha límite debe ser almenos 15 años atrás`
+  }),
+  curp: z.string().refine(curp =>{
+    const regexCurp = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/;
+    
+    if(!regexCurp.test(curp)){
+      return "C.R.P. inválida, favor de revisar"
+    }
+    return;
+  }),
+  estadoCivil: z.nativeEnum(EstadoCivil, {
+    errorMap: (issue, ctx) => {
+      console.log(issue, ctx);
+      if (issue.code === "invalid_enum_value")
+        return {
+          message: "Opción no válida.",
+        };
+      return {
+        message: "Debes seleccionar una opción.",
+      };
+    },
+  }),
+  dialecto: z.nativeEnum(Dialecto, {
     errorMap: (issue, ctx) => {
       console.log(issue, ctx);
       if (issue.code === "invalid_enum_value")
