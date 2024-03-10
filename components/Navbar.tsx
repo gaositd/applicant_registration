2de69a8b9397769551de9032bece28ecd53d994d
Jonathan Marquez <zomodan2306@gmail.com>
2024-03-10 10:54:51 -0600

    fix(lint): fixed lint errors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,21 @@
-'use client'
+"use client";
 
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
   MenuList,
+  useDisclosure,
   useToast,
-  Image,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  DrawerHeader,
-  DrawerBody,
-  DrawerFooter,
-  useDisclosure
-} from '@chakra-ui/react'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import { AiFillCaretDown } from 'react-icons/ai'
-import AppConfigDrawer from './navbar/AppConfigDrawer'
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { AiFillCaretDown } from "react-icons/ai";
+import AppConfigDrawer from "./navbar/AppConfigDrawer";
 
 interface props {
   isAdmin: boolean;
@@ -32,62 +24,55 @@ interface props {
 const Navbar: React.FC<props> = ({ isAdmin }) => {
   const [user, setUser] = useState<{ name: string; matricula: string } | null>(
     null
-  )
+  );
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
-    const user = localStorage.getItem('user')
+    const user = localStorage.getItem("user");
 
-    if (user) setUser(JSON.parse(user))
-  }, [])
+    if (user) setUser(JSON.parse(user));
+  }, []);
 
   const toast = useToast();
 
   const handleLogout = () => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
-      method: 'DELETE',
-      credentials: 'include'
+      method: "DELETE",
+      credentials: "include",
     }).then((res) => {
       if (res.status === 200) {
-        localStorage.removeItem('user')
+        localStorage.removeItem("user");
 
-        window.location.assign('/login')
+        window.location.assign("/login");
       } else {
         toast({
-          description: 'No se puede cerrar la sesion',
-          status: 'error',
-          title: 'Ha sucedido un error al cerrar la sesion'
-        })
+          description: "No se puede cerrar la sesion",
+          status: "error",
+          title: "Ha sucedido un error al cerrar la sesion",
+        });
       }
-    })
-  }
+    });
+  };
 
   return (
-    <Flex
-      bgColor='primary.base'
-      w='100%'
-      h='10%'
-      p={6}
-      justify='space-between'
-      align='center'
-    >
+    <Flex bgColor="primary.base" w="100%" h="10%" p={6} align="center">
       <Flex>
-        <Image src='/logo.png' alt='logo' />
+        <Image src="/logo.png" alt="logo" />
       </Flex>
 
       <div>
-        <Heading color='white' size='md'>
+        <Heading color="white" size="md">
           Sistema de preinscripciones
         </Heading>
       </div>
       <Menu>
         <MenuButton
-          id='3'
+          id="3"
           as={Button}
           rightIcon={<AiFillCaretDown />}
           borderRadius={10}
-          colorScheme='white'
+          colorScheme="white"
         >
           {user?.name}
         </MenuButton>
@@ -109,7 +94,7 @@ const Navbar: React.FC<props> = ({ isAdmin }) => {
             Perfil
           </MenuItem>
           {isAdmin && (
-            <MenuItem id='3' onClick={onOpen}>
+            <MenuItem id="3" onClick={onOpen}>
               Ajustes
             </MenuItem>
           )}
@@ -121,7 +106,7 @@ const Navbar: React.FC<props> = ({ isAdmin }) => {
       </Menu>
       {isAdmin && <AppConfigDrawer isOpen={isOpen} onClose={onClose} />}
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
