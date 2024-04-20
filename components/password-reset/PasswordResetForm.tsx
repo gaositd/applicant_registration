51cf9d493b3bc6a3804b541db4d971d158504d9f
Jonathan Marquez <zomodan2306@gmail.com>
2024-04-19 19:48:16 -0600

    feat(users): updated event to change password on client

--- components/password-reset/PasswordResetForm.tsx
+++ components/password-reset/PasswordResetForm.tsx
@@ -12,7 +12,7 @@ import { useState } from "react";
 import AuthLayout from "../pages/rgister/AuthLayout";
 import { Loading } from "../loadingComponent";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 type PasswordResetFormProps = {
   token: string | null;
@@ -58,7 +58,7 @@ const FormWithouToken = () => {
           headers: {
             "Content-Type": "application/json",
           },
-        },
+        }
       );
       if (response.ok) {
         toast({
@@ -114,15 +114,90 @@ type FormWithTokenProps = {
   token: string;
 };
 const FormWithToken: React.FC<FormWithTokenProps> = ({ token }) => {
-  const { isLoading } = useQuery("verify-password-reset-token", async () =>
-    axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/verify-password-reset-token?token=${token}`,
-    ),
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const toast = useToast();
+  const { mutate } = useMutation(
+    "reset-passsword",
+    async (data: { password: string }) => {
+      console.log(data, token);
+      return axios.put(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/password/${token}`,
+        data
+      );
+    },
+    {
+      onSuccess: () => {
+        toast({
+          title: "Clave cambiada",
+          description: "Tu clave ha sido cambiada exitosamente",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+          onCloseComplete() {
+            window.location.replace("/login");
+          },
+        });
+      },
+      onError: () => {
+        toast({
+          title: "Error",
+          description: "No se pudo cambiar la clave",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      },
+    }
   );
+
+  const { isLoading } = useQuery(
+    "verify-password-reset-token",
+    async () =>
+      axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/password-reset-token?token=${token}`
+      ),
+    {
+      onSuccess: () => setValidToken(true),
+      onError: () => setValidToken(false),
+    }
+  );
+
+  const [validToken, setValidToken] = useState(false);
 
   if (isLoading) {
     return <Loading />;
   }
+
+  if (!validToken) {
+    return (
+      <Flex flexDir={"column"} width="100%" alignItems="center" gap={4}>
+        <Heading size={"md"} color={"white"}>
+          El token no es valido, solicita un nuevo token
+        </Heading>
+        <Button
+          colorScheme="teal"
+          onClick={() => window.location.replace("/password-reset")}
+        >
+          Solicitar nuevo token
+        </Button>
+      </Flex>
+    );
+  }
+  const handlePasswordReset = () => {
+    if (password !== confirmPassword) {
+      toast({
+        title: "Error",
+        description: "Las contraseñas no coinciden",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return;
+    }
+    mutate({ password });
+  };
 
   return (
     <Flex flexDir={"column"} width="100%" alignItems="center" gap={4}>
@@ -135,6 +210,8 @@ const FormWithToken: React.FC<FormWithTokenProps> = ({ token }) => {
           id="password"
           placeholder="********"
           bgColor="white"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </FormControl>
       <FormControl>
@@ -145,11 +222,14 @@ const FormWithToken: React.FC<FormWithTokenProps> = ({ token }) => {
           type="password"
           id="password"
           placeholder="********"
-          color="white"
           bgColor="white"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </FormControl>
-      <Button colorScheme="teal">Cambiar Clave</Button>
+      <Button colorScheme="teal" onClick={handlePasswordReset}>
+        Cambiar Clave
+      </Button>
     </Flex>
   );
 };
